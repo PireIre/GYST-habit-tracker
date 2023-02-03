21de5376fcafbf6fcf131cf8bcbb45d1e6fb0aac
Irenej <irenej@bambuser.com>
2023-02-03 08:23:51 +0100

    Apply router to some pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,19 @@
-import React, { useEffect, useState } from "react"
-import Habit from "./components/Habit"
+import React, { useEffect, useState } from "react";
+import Habit from "./components/Habit";
+import { Route, Routes } from "react-router-dom";
+import LoginPage from "./components/LoginPage"
+import NotFound from "./components/NotFound"
+
 
 function App() {
   return (
-    <div>
-      <Habit /> 
-    </div>
+    <main>
+      <Routes>
+        <Route path="/" element={<Habit />}></Route>
+        <Route path="/login" element={<LoginPage />}></Route>
+        <Route path="/not-found" element={<NotFound />}></Route>
+      </Routes>
+    </main>
   );
 }
 
