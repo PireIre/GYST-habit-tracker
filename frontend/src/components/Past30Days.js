5928aecaf6e41e3602522174add24052ace481a5
Irenej <irenej@bambuser.com>
2023-02-11 09:44:38 +0100

    Add component that puts out past 30 days for keeping track of habits

---
+++ frontend/src/components/Past30Days.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react";
+import "./Past30Days.css";
+import logo from "../static/Logo.png"
+
+const Past30Days = () => {
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    const today = new Date();
+    const pastDays = [];
+    for (let i = 0; i < 30; i++) {
+      const day = new Date(today);
+      day.setDate(today.getDate() - i);
+      pastDays.push(
+        `${day.toLocaleString("default", { month: "long" })} ${day.getDate()}, ${day.toLocaleString("default", { weekday: "long" })}`
+      );
+    }
+    setDays(pastDays);
+  }, []);
+
+  return (
+    <>
+    <img width="160px" src={logo}></img> <br/><br/>
+    <ul className="past-30-days">
+      {days.map((day, index) => (
+        <li key={index} className="past-30-days__item">{day}</li>
+      ))}
+    </ul>
+    </>
+  );
+};
+
+export default Past30Days;
