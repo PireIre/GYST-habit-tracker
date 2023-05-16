b135f916ce24b598754ec1751775ecbb3171dc30
Irenej <irenej@bambuser.com>
2023-05-16 14:27:34 +0200

    Update and style 404 page

--- frontend/src/components/NotFound.js
+++ frontend/src/components/NotFound.js
@@ -1,9 +1,13 @@
-import React from "react"
+import React from 'react';
+import './NotFound.css';
 
 const NotFound = () => {
   return (
-    <h1>Not found</h1>
-  )
-}
+    <div className="not-found-page">
+      <h1 className="not-found-title">404 - Page Not Found</h1>
+      <p className="not-found-paragraph">The page you are looking for does not exist.</p>
+    </div>
+  );
+};
 
 export default NotFound;
