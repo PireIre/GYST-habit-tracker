d3cf1fb07c8e0d9f55d7d1ab2140a557d889168d
Irenej <irenej@bambuser.com>
2023-05-18 10:20:15 +0200

    Populate real username in navbar

--- frontend/src/components/GystNavbar.js
+++ frontend/src/components/GystNavbar.js
@@ -1,13 +1,27 @@
-import React, { useContext } from "react";
+import React, { useContext, useState} from "react";
 import { Navbar } from "react-bootstrap";
 import "./GystNavbar.css";
 import { useNavigate } from 'react-router-dom';
 import { AuthContext } from './AuthContext';
 
 
+
 function GystNavbar() {
   const navigate = useNavigate();
   const { isAuthenticated, setIsAuthenticated } = useContext(AuthContext);
+  
+  const [username, setUsername] = useState("")
+
+  if (isAuthenticated) {
+    fetch("/api/user/me", {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'x-auth-token': window.localStorage.getItem('x-auth-token')
+      }})
+      .then(res => res.json())
+      .then(user => setUsername(user.name))
+  }
 
 
   const handleLogout = () => {
@@ -22,7 +36,7 @@ function GystNavbar() {
       <Navbar.Collapse className="justify-content-end">
         {isAuthenticated &&
           <Navbar.Text>
-            <div>Ire Pire</div>
+            <div>{username}</div>
             <div style={{ fontWeight: "bold", cursor: "pointer" }} onClick={handleLogout} href="/login">Log out</div>
           </Navbar.Text>
         }
